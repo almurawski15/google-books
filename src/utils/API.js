92d28842b87b9f57fa07cfa591f6b2fbec6337cc
Al Murawski <almurawski15@gmail.com>
2021-06-22 22:10:07 -0400

    updated readme

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getBook: function (query) {
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`):
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
     },
 };
