a10055ffaa83eac2caab9d89029ff00afbcb6c96
Al Murawski <almurawski15@gmail.com>
2021-06-23 15:54:28 -0400

    created search and save pages, need to pull api data to form

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-function SearchForm () {
+function SearchForm(props) {
     return (
-        <div>
-            <h1> searches </h1>
+        <div className="container">
+        <form>
+            <div className="form-group">
+            <label htmlFor="search"><h2>Search and Save Your Favorite Books</h2></label>
+            <input
+                onChange={props.handleInputChange}
+                value={props.search}
+                name="search"
+                type="text"
+                className="form-control"
+                placeholder="Search a Book"
+                id="search"
+            />
+            <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3 mb-5">
+                Search
+                </button>
+            </div>
+        </form>
         </div>
     )
 }
