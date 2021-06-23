c9fe6e408f42fea8c586fc80df2df41ccfb9a78e
Al Murawski <almurawski15@gmail.com>
2021-06-23 15:53:52 -0400

    updated navbar links and styling

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 function Navbar() {
     return (
-        <div>
-            <h1> nav </h1>
-        </div>
+        <nav className="navbar navbar-expand-lg text-light">
+            <div className="nav-container">
+                <Link className="navbar-title" to="/">Google Books</Link>
+                <ul className="navbar-links">
+                    <li className="nav-search">
+                    <Link className="nav-link" to="/search">Search</Link>
+                    </li>
+                    <li className="nav-saved">
+                    <Link className="nav-link" to="/saved">Saved</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
