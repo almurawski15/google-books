f1cc32aff58338e2d2cb596443264fa4c3174a66
Al Murawski <almurawski15@gmail.com>
2021-06-23 16:04:04 -0400

    ran build

---
+++ client/build/precache-manifest.7f01e93b48bf435d4b2a0c65ed4aaa76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b95d31fea1dcfd6ac1d",
+    "url": "/static/css/main.9c162bf7.chunk.css"
+  },
+  {
+    "revision": "9b95d31fea1dcfd6ac1d",
+    "url": "/static/js/main.cb7a334e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5cb818b88365b4042720",
+    "url": "/static/js/2.ebfeec5a.chunk.js"
+  },
+  {
+    "revision": "f00e871b052543909a1547581f17c645",
+    "url": "/static/media/beautiful-library.f00e871b.jpg"
+  },
+  {
+    "revision": "ac1372a8c898d7b025a7a7026cc6b790",
+    "url": "/index.html"
+  }
+];
