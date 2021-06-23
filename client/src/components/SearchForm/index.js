bcc20b4edcb248dfb03f952fdc2e6585176b73c8
Al Murawski <almurawski15@gmail.com>
2021-06-23 12:47:58 -0400

    added client package.json and created components

---
+++ client/src/components/SearchForm/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchForm () {
+    return (
+        <div>
+            <h1> searches </h1>
+        </div>
+    )
+}
+
+export default SearchForm;
+
--- src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default {
-    getBook: function (query) {
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-    },
-};
